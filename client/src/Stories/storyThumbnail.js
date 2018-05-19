7031d3056dbc535ae7a8ad6250d5edd282108091
Hilary Kitz <hilarykitz1@Small-Rat.fritz.box>
2018-05-19 21:25:39 +0200

    hide broken imgs

--- client/src/Stories/storyThumbnail.js
+++ client/src/Stories/storyThumbnail.js
@@ -12,12 +12,16 @@ const buildThumbnail = ({
   urlToImage,
   publishedAt
 }) => {
+  const hideImage = e => {
+    e.target.style.display = "none";
+  };
+
   return description && urlToImage ? (
     <div key={url} className="storyThumbnail">
       <a href={url} target="blank">
         <div className="storyDescription">
           <h2>{title}</h2>
-          <img alt={title} src={urlToImage} />
+          <img onError={e => hideImage(e)} alt={title} src={urlToImage} />
           <p>{description}</p>
         </div>
       </a>
